3789fcef6dabc261a4bcba9fdb66f2c9db6afa51
Arturo <a.balbin.r@gmail.com>
2020-07-20 22:20:19 -0500

    Sacffold EF

---
+++ Backend/WebDeveloper.DbOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace WebDeveloper.DbOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var context = new chinookContext();
+            var albums = context.Album.ToList();
+            foreach (var album in albums)
+            {
+                Console.WriteLine($"Nombre del album: {album.Title}");
+                Console.WriteLine("Nombre del album: " + album.Title);
+            }
+        }
+    }
+}
