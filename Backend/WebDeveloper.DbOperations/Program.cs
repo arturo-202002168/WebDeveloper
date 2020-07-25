9558ce6c57c7ef50bb4f8072e01b01b82607a26f
Arturo <a.balbin.r@gmail.com>
2020-07-24 22:16:33 -0500

    add mvc

--- Backend/WebDeveloper.DbOperations/Program.cs
+++ Backend/WebDeveloper.DbOperations/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
+using WebDeveloper.Infra.Data;
 
 namespace WebDeveloper.DbOperations
 {
@@ -8,8 +10,12 @@ namespace WebDeveloper.DbOperations
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var context = new chinookContext();
-            var albums = context.Album.ToList();
+            // Instanciar el contexto de EF
+            var options = new DbContextOptionsBuilder<ChinookContext>()
+                .UseSqlServer("server=.;database=Chinook;trusted_connection=true;")
+                .Options;
+            var context = new ChinookContext(options);
+            var albums = context.Albums.ToList();
             foreach (var album in albums)
             {
                 Console.WriteLine($"Nombre del album: {album.Title}");
---
+++ Backend/WebDeveloper.Mvc/Controllers/ReportesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebDeveloper.Core.ViewModels;
+using WebDeveloper.Infra.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebDeveloper.Mvc.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private readonly ChinookContext _context;
+        public ReportesController(ChinookContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/reportes/albumes-por-artista")]
+        public IActionResult AlbumesPorArtista()
+        {
+            var items = _context.Artists.Select(a => new AlbumPorArtistaItemViewModel
+            {
+                ArtistId = a.ArtistId,
+                ArtistName = a.Name,
+                CantidadAlbumes = a.Albums.Count()
+            }).ToList();
+
+            // Crear el objeto view model que enviaremos a la vista
+            var model = new AlbumPorArtistaViewModel { Title = $"Se tienen {items.Count}", Items = items };
+            return View(model);
+        }
+
+        [Route("/reportes/albumes-por-artista-sp")]
+        public IActionResult AlbumesPorArtistaSP()
+        {
+            var items = _context.ArtistCounts.FromSqlRaw("sp_algo").ToList();
+
+            // Crear el objeto view model que enviaremos a la vista
+            var model = new AlbumPorArtistaViewModel
+            {
+                Title = $"Se tienen {items.Count}",
+                Items = items.Select(a => new AlbumPorArtistaItemViewModel
+                {
+                    ArtistId = a.ArtistId,
+                    ArtistName = a.Nombre,
+                    CantidadAlbumes = a.Cantidad
+                }).ToList()
+            };
+            return View("AlbumesPorArtista", model);
+        }
+    }
+}
